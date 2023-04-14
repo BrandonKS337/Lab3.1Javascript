017bcf58a0ff4d4ca4e359f35fb2b4bfb4ec6f4c
Project V2 <brandonks337@gmail.com>
2023-04-13 21:16:34 -0500

    adding part 1

---
+++ module3.js
@@ -0,0 +1,19 @@
+// // "" + 1 + 0                               //1: 1       "" defines line to be a string
+// // "" - 1 + 0                               //2: -1        
+// // true + false                             //3: 1
+// !true                                       //4: false
+// 6/"3"                                        //5: 2
+// "2"*3                                       //6: 23
+// 4+5+"px"                                    //7: 6
+// "$" + 4 + 5                                 //8: $45
+// "4" - 2                                     //9: 2
+// "4px" - 2                                   //10: NaN     number and letters same string cause it to fail
+// " -9 " + 5                                  //11:  -9 5
+// " -9 " - 5                                  //12: =14
+// null + 1                                    //13: 1
+// undefined + 1                               //14: NaN
+// undefined == null                           //15: True
+// undefined === null                          //16: False
+// " \t \n" - 2                                //17: -2
+
+// console.log(" \t \n" - 2) 
